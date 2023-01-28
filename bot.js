2645216f457cbd690720758f05e2e1ae250b1cf8
Rushwayc <rushwayc@yahoo.com>
2023-01-28 23:57:00 +0100

    Connected scrapping functionality and Discord bot notifications.

--- bot.js
+++ bot.js
@@ -7,7 +7,12 @@ const puppeteer = require("puppeteer");
 
 client.on("ready", () => {
   console.log("I am ready!");
-  (async () => {
+
+  let previous_content = [];
+  let new_content = [];
+
+  async function scrape() {
+    //Puppeteer code.
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.setUserAgent(
@@ -17,7 +22,6 @@ client.on("ready", () => {
       "https://www.upwork.com/nx/jobs/search/?q=customer%20support&t=0&contractor_tier=3&hourly_rate=15-&from_recent_search=true&sort=recency"
     );
     await page.waitForTimeout(5000);
-
     const jobPosts = await page.evaluate(() =>
       Array.from(
         document.getElementsByClassName("up-card-list-section"),
@@ -31,8 +35,46 @@ client.on("ready", () => {
     );
 
     await browser.close();
-    console.log(jobPosts);
-  })();
+    //Puppeteer code end.
+
+    jobPosts.forEach(function (jobPosts) {
+      let content = jobPosts;
+      new_content.push(content);
+    });
+
+    function compareArrays(a, b) {
+      // If length is not equal
+      if (a.length != b.length) return "False";
+      else {
+        // Comparing each element of array
+        for (var i = 0; i < a.length; i++) if (a[i] != b[i]) return "False";
+        return "True";
+      }
+    }
+    let compareResults = compareArrays(previous_content, new_content);
+
+    if (previous_content.length == 0) {
+      previous_content = new_content;
+      console.log("Previous content is empty.");
+    } else {
+      if (!compareResults) {
+        console.log("New job found!");
+        client.users.fetch("181195607887708161", false).then((user) => {
+          user.send(
+            `------------------------\n*New job post found!*\n\n**Title**: ${jobPosts[0].title}\n**Hourly**: ${jobPosts[0].hourly}\n\n**Description**: ${jobPosts[0].description}\n\n**Link**: ${jobPosts[0].link}`
+          );
+        });
+      } else {
+        console.log(
+          "No new job posts, waiting 60 seconds before checking again."
+        );
+      }
+    }
+    previous_content = new_content;
+    new_content = [];
+  }
+  setInterval(scrape, 6000);
 });
 
-client.login("Insert token.");
+//Discord bot token.
+client.login("Insert token here.");
